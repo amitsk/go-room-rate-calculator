3eb10caf847e56ad2b8db6644c5f164b3971071f
Amit K <amitsk1974@gmail.com>
2022-12-05 16:48:44 -0800

    set up viper for configurations

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/amitsk/go-room-rate-calculator/pkg/adapters"
 	"github.com/amitsk/go-room-rate-calculator/pkg/ratecalculation"
+	"github.com/spf13/viper"
 )
 
 func main() {
@@ -16,6 +18,47 @@ func main() {
 }
 
 func run() error {
+	viper.SetConfigName("config") // config file name without extension
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("./config/") // config file path
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv() // read value ENV variable
+
+	// 	Example config:
+
+	// module:
+	//     enabled: true
+	//     token: 89h3f98hbwf987h3f98wenf89ehf
+	// */
+	// type config struct {
+	// 	Module struct {
+	// 		Enabled bool
+
+	// 		moduleConfig `mapstructure:",squash"`
+	// 	}
+	// }
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; ignore error if desired
+			fmt.Println("No config file: default \n", err)
+			os.Exit(1)
+		} else {
+			// Config file was found but another error was produced
+			fmt.Println("fatal error config file: default \n", err)
+			os.Exit(1)
+		}
+	}
+	// Set default value
+
+	pgUser := viper.GetString("database.roomrate.user")
+	pgHost := viper.GetString("database.roomrate.host")
+	pgPort := viper.GetString("database.roomrate.port")
+	pgPwd := viper.GetString("database.roomrate.pwd")
+
+	// Declare vars
+
 	db, err := adapters.SetupDatabase("postgres")
 	if err != nil {
 		return err
